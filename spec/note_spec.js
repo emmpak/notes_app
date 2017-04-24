7463db923cfb06c8c113045e539abc26c57283af
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-24 16:40:50 +0100

    First test added for creating text with note at unit level

--- spec/note_spec.js
+++ spec/note_spec.js
@@ -1,1 +1,6 @@
 // var assert = require("./testing_library/assert").assert
+
+function testContentOfNote() {
+  var note = new Note("This is a note!");
+  assert.isTrue(note.text === "This is a note!");
+};
