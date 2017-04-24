dfd529272b9390d99d889d9df7ace8f27fc3a896
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-24 17:13:04 +0100

    refactor to use module pattern and create html page

--- note-model.js
+++ note-model.js
@@ -1,3 +1,7 @@
-function Note(text) {
-  this.text = text;
-}
+(function(exports) {
+  function Note(text) {
+    this.text = text;
+  };
+
+  exports.Note = Note;
+})(this);
--- spec/note_spec.js
+++ spec/note_spec.js
@@ -1,6 +1,8 @@
 // var assert = require("./testing_library/assert").assert
-
-function testContentOfNote() {
-  var note = new Note("This is a note!");
-  assert.isTrue(note.text === "This is a note!");
-};
+(function(exports) {
+  function testContentOfNote() {
+    var note = new Note("This is a note!");
+    assert.isTrue(note.text === "This is a note!");
+  };
+  testContentOfNote();
+})(this);
