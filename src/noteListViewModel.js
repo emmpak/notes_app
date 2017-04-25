de0acb7e01376fb6400ff858b3f9026f852c4201
emmpak <eiliev05@yahoo.com>
2017-04-25 16:52:07 +0100

    Add getAllNotes html function

--- src/noteListViewModel.js
+++ src/noteListViewModel.js
@@ -1,7 +1,20 @@
 (function(exports) {
   function NoteListView(noteList) {
     this.notesRepo = noteList;
+    this.getAllNotes = function() {
+      // this.NotesRepo.allNotes.join("<li><div>")
+      var html = []
+      for(var i=0; i < this.notesRepo.allNotes.length; i++) {
+        html.push('<li><div>' + this.notesRepo.allNotes[i].text + '</div></li>');
+      }
+      return '<ul>' + html.join('') + '</ul>';
+    }
   }
 
   exports.NoteListView = NoteListView;
 })(this);
+
+
+
+
+var html = "<ul><li><div>Hello</div></li><li><div>World</div></li></ul>";
