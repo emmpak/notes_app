e11aeead1d489b8b8e8c1ac53ae649cf2efbc0be
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-26 12:56:08 +0100

    page renders single note when link clicked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 list = new NoteList();
-// list.saveNote("Favourite drink: seltzer");
-// list.saveNote("Will loves JavaScript");
+list.saveNote("Favourite drink: seltzer");
+list.saveNote("Will loves JavaScript");
 
-controller = new NoteController(list);
+var controller = new NoteController(list);
 controller.setupNoteList();
+
+makeUrlChangeShowNoteForCurrentPage();
+
+function makeUrlChangeShowNoteForCurrentPage() {
+  window.addEventListener("hashchange", showNoteForCurrentPage);
+};
+
+function showNoteForCurrentPage() {
+  controller.showNote(list.getSpecificNote(getNoteFromURL(window.location)));
+};
+
+function getNoteFromURL(location) {
+  return location.hash.split("/")[1];
+}
